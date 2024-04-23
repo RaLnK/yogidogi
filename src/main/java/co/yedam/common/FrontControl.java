bce2c3f4306e03c9ed55765237de23c0dd25fe18
ksh <admin@YD05-10>
2024-04-23 09:38:29 +0900

    240423

--- src/main/java/co/yedam/common/FrontControl.java
+++ src/main/java/co/yedam/common/FrontControl.java
@@ -10,12 +10,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-import co.yedam.board.BoardList;
-import co.yedam.board.addBoard;
 import co.yedam.control.AddCartControl;
 import co.yedam.control.CartListControl;
+import co.yedam.control.ModifyCartControl;
 import co.yedam.control.RemoveCartControl;
+import co.yedam.control.TestControl;
 
 public class FrontControl extends HttpServlet {
 	Map<String, Control> map;
@@ -26,24 +25,15 @@ public class FrontControl extends HttpServlet {
 
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-
-
-		
-		//게시판 관련
-		FrontControlBoard.push(map);
-
-
+		map.put("/test.do", new TestControl());
 		// 장바구니
 		map.put("/cartList.do", new CartListControl()); // 카트페이지 이동
 		map.put("/removeCart.do", new RemoveCartControl()); // 카트삭제
-		//map.put("/modifyCart.do", new ModifyCartControl()); // 카트수정
+		map.put("/modifyCart.do", new ModifyCartControl()); // 카트수정
 		map.put("/addCart.do", new AddCartControl()); // 카트등록
-
-		FrontControlProd.push(map);
-
+		
 		//로그인
 		FrontControlLogin.push(map);
-
 	}
 
 	@Override
