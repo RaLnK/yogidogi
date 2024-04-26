fc03529e6a9e68a7646a46c63e597e40915c71d4
yg <admin@yedam.com>
2024-04-26 17:32:25 +0900

    240426_yg

--- src/main/java/co/yedam/board/AddBoard.java
+++ src/main/java/co/yedam/board/AddBoard.java
@@ -19,7 +19,7 @@ public class AddBoard implements Control {
 
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		//생성자 매개값 1. 요청정보 2. 저장경로 3. 최대크기 4.인코딩 5.리네임정책 
 		String savePath = req.getServletContext().getRealPath("images"); 
 		int maxSize = 1024 * 1024 * 5;
 		MultipartRequest multi = new MultipartRequest(req,savePath,maxSize,"utf-8",new DefaultFileRenamePolicy());
--- src/main/java/co/yedam/service/BoardService.java
+++ src/main/java/co/yedam/service/BoardService.java
@@ -6,6 +6,7 @@ import co.yedam.board.Board;
 import co.yedam.vo.BoardVO;
 
 public interface BoardService {
+	List<BoardVO> nbList();
 	List<BoardVO> boardList();
 	boolean addBoard(BoardVO bvo);
 	BoardVO getBoard(int bno);
