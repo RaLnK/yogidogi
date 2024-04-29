21c9e793c1843a92ea2d3e330d99d398ce506596
yg <admin@yedam.com>
2024-04-29 12:47:40 +0900

    mybatis update

--- src/main/java/co/yedam/service/ReplyServiceImpl.java
+++ src/main/java/co/yedam/service/ReplyServiceImpl.java
@@ -2,15 +2,22 @@ package co.yedam.service;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+
+import co.yedam.common.DataSource;
+import co.yedam.mapper.ReplyMapper;
 import co.yedam.vo.PageVO;
 import co.yedam.vo.ReplyVO;
 
 public class ReplyServiceImpl implements ReplyService {
-
+SqlSession session = DataSource.getInstance().openSession(true);
+	
+	ReplyMapper mapper = session.getMapper(ReplyMapper.class);
+	
 	@Override
 	public List<ReplyVO> replyList(PageVO pg) {
 		// TODO Auto-generated method stub
-		return null;
+		return mapper.replyList(pg);
 	}
 
 }
