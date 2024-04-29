d0c66e31268ef445545606550996cf9965b7ce99
ksh <admin@YD05-10>
2024-04-29 17:42:20 +0900

    240429/footer

--- src/main/webapp/WEB-INF/mainLayouts/footer.jsp
+++ src/main/webapp/WEB-INF/mainLayouts/footer.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<script src="./js/footer.js"></script>
+<script src="./js/secFooter.js"></script>
 <footer class="footer-section">
 	<div class="container relative">
 		<div class="sofa-img">
---
+++ src/main/webapp/js/secFooter.js
@@ -0,0 +1,18 @@
+/**
+ * section.js
+ */
+console.log('footer');
+let keyword = document.querySelector('.form-control').value;
+$('.btn-primary').on('click', searchKeyword);
+
+function searchKeyword() {
+	if(document.querySelector('.g-3 option').value == '상품') {
+		
+	}else if(document.querySelector('.g-3 option').value == '공지사항') {
+		
+	}else if(document.querySelector('.g-3 option').value == '문의사항') {
+		
+	}else if(document.querySelector('.g-3 option').value == '게시글') {
+		
+	}
+};
